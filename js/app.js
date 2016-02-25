6b2f37ff378e1569e6c698c25c1453c42b20db6d
Teisia Park <teisia@gmail.com>
2016-02-25 16:19:52 -0700

    search bar working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 var app = angular.module('madTea', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html',
+      controller: 'mainController'
+  })
+});
